0e045243605c6db0f28ebe9ec0fd848b606952f7
Nguyen Truong Duy <truongduy134@yahoo.com>
2014-05-10 16:19:48 -0700

    Update solution of 11614

--- Volume_116/UVA11614.cpp
+++ Volume_116/UVA11614.cpp
@@ -3,17 +3,30 @@
  ********************************************************************************/
 
 /* Methodology:
+ *
+ * Method 1:
  *  + Use binary search
  *  + Let T(n) = n * (n + 1) / 2 (The n-th triangle number)
  *  + Let M be the number of warriors.
- *  + Then the answer to the problem is the smallest n such that T(n) > M
+ *  + Then the answer to the problem is the largest n such that T(n) <= M
  *
  *  + Note that n = 1414213562 is the smallest n such that T(n) > 10^18
  *  + Therefore, for each input M, we can binary search on [0, 1414213562] to find the answer.
- *  + Corner case: If n = 0, then the number of rows is 0 
+ *  + Corner case: If M = 0, then the number of rows is 0 
+ *
+ * Method 2: Closed formula
+ *  + If M = 0, then the answer is 0
+ *  + Consider M >= 0
+ *  + Suppose n is the number of rows that can be formed.
+ *    Then we have n * (n + 1) / 2 <= M
+ *          => n^2 + n - 2M <= 0
+ *    We have delta = 1^2 - 4 * (-2M) = 1 + 8 * M
+ *    So n <= (-1 + sqrt(1 + 8 * M)) / 2
+ *    Therefore, the largest n satisfied (the largest number of rows that can be formed) is n = floor((-1 + sqrt(1 + 8 * M)) / 2)
  */
 
 #include <cstdio>
+#include <cmath>
 
 using namespace std;
 
@@ -51,6 +64,16 @@ int binarySearchNumRow(unsigned long long numPeople) {
 
     return -1;  // Dummy
 }
+
+/* Method 2: Closed formula
+int calculateNumRow(unsigned long long numPeople) {
+    if (numPeople == 0) {
+        return 0;
+    }
+
+    return (int) floor((-1 + sqrt(1 + 8 * numPeople)) / 2);
+}
+*/
 
 int main(void) {
     int numTest;
