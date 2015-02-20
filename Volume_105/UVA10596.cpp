3c3f1e6f8498ad913ba37c1cc9a6a46b683cd882
Nguyen Truong Duy <truongduy134@yahoo.com>
2015-02-21 00:36:21 +0700

    Update the correct solution of UVA10596

--- Volume_105/UVA10596.cpp
+++ Volume_105/UVA10596.cpp
@@ -1,77 +1,95 @@
+/*******************************************************
+ * Author: Nguyen Truong Duy
+ *******************************************************/
+
+//// C++11 ////
+
+/**
+ * Methodology:
+ * - The problem essentially asks if there exists an Euler tour that visits
+ * all edges in the given graph
+ * - A graph has an Euler tour if and only if it is connected and every vertex
+ * has an even degree (*)
+ * - Gotchas:
+ *  + For this problem, we should exclude isolated vertices when checking
+ * connectivity. That means if the graph has 1 connected component whose
+ * number of edges is larger than 0 and every other vertices (not in this
+ * connected component) are isolated vertices, then connectivity requirement
+ * in (*) is satisfied
+ *  + If the number of edges is 0, then the ouput should be "Not Possible"
+ *
+ * - Time complexity: O(V + E)
+ */
+
 #include <cstdio>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
-bool isConnect(const vector<vector<int> > & adjList)
-{
-    int numV = (int) adjList.size(), anotherV;
-
-    if(numV == 0)
-        return true;
-
-    vector<bool> visited(numV, false);
-    queue<int> bfsQueue;
-
-    bfsQueue.push(0);
-
-    while(!bfsQueue.empty())
-    {
-        anotherV = bfsQueue.front();
-        bfsQueue.pop();
-        visited[anotherV] = true;
-
-        for(int i = 0; i < (int) adjList[anotherV].size(); i++)
-            if(!visited[adjList[anotherV][i]])
-            bfsQueue.push(adjList[anotherV][i]);
+void dfs(
+    const vector<vector<int> >& adjList,
+    int startV,
+    vector<bool>& visited) {
+  visited[startV] = true;
+  for (auto&& neighborV : adjList[startV]) {
+    if (!visited[neighborV]) {
+      dfs(adjList, neighborV, visited);
     }
-
-    for(int i = 0; i < numV; i++)
-        if(!visited[i])
-            return false;
-
-    return true;
+  }
 }
 
-bool isEulerian(const vector<vector<int> > & adjList)
-{
-    for(int i = 0; i < (int) adjList.size(); i++)
-        if(adjList[i].size() & 1)
-            return false;
+bool existEulerTour(const vector<vector<int> >& adjList) {
+  // Checks if every vertex has even degree
+  for (auto&& neighbors : adjList) {
+    if (neighbors.size() & 1) {
+      return false;
+    }
+  }
 
-    return isConnect(adjList);
+  // Checks if the graph is connected
+  vector<bool> visited(adjList.size(), false);
+  // Excludes all isolated vertices in our traversal
+  for (int v = 0; v < adjList.size(); ++v) {
+    if (!adjList[v].size()) {
+      visited[v] = true;
+    }
+  }
+  int startV = -1;
+  for (int v = 0; v < visited.size(); ++v) {
+    if (!visited[v]) {
+      startV = v;
+      break;
+    }
+  }
+  if (startV >= 0) {
+    dfs(adjList, startV, visited);
+  }
+  for (auto&& boolValue : visited) {
+    if (!boolValue) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-int main(void)
-{
-    int numV, numE, u, v;
-    vector<vector<int> > adjList;
+int main(void) {
+  int numV, numE, u, v;
+  vector<vector<int> > adjList;
 
-  //  freopen("in.txt", "r", stdin);
-
-    while(scanf("%d %d", &numV, &numE) > 0)
-    {
-        adjList.clear();
-
-        for(int i = 0; i < numV; i++)
-        {
-            vector<int> neighborList;
-            adjList.push_back(neighborList);
-        }
-
-        for(int i = 0; i < numE; i++)
-        {
-            scanf("%d %d", &u, &v);
-            adjList[u].push_back(v);
-            adjList[v].push_back(u);
-        }
-
-        if(isEulerian(adjList))
-            printf("Possible\n");
-        else
-            printf("Not Possible\n");
+  while (scanf("%d %d", &numV, &numE) > 0) {
+    adjList.assign(numV, vector<int>());
+    for (int i = 0; i < numE; ++i) {
+      scanf("%d %d", &u, &v);
+      adjList[u].push_back(v);
+      adjList[v].push_back(u);
     }
 
-    return 0;
+    if (numE && existEulerTour(adjList)) {
+      printf("Possible\n");
+    } else {
+      printf("Not Possible\n");
+    }
+  }
+  return 0;
 }
